f3e5725df00e82e9d56df405d446ba61333f22e9
Dhruvil-vjshree <dhruvil09.dp@gmail.com>
2023-04-26 11:07:55 +0530

    task_2_complete

--- lib/main.dart
+++ lib/main.dart
@@ -9,53 +9,125 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
         child:Column(
           children: [
-
-            Expanded(
-              flex: 8,
-                child:Container(
-                  constraints: BoxConstraints.expand(),
-                  color: Colors.red,
-                  child: Center(
-                      child: Text('8',
-                        style: TextStyle(
-                          color  : Colors.white,
-                          fontSize: 30
-                        )
-                      )
-                  ),
-                )
+            _customContainer(
+                number: 8,
+            color: Colors.red,
             ),
             Expanded(
               flex: 5,
-                child:Container(
-                  constraints: BoxConstraints.expand(),
+              child:Row(
+                children:[
+                  _customContainer(
+                number: 5,
+                color: Colors.blue,
+              ),
 
-                  color: Colors.indigo,
 
-                    child: Center(
-                      child:Text('5',
-                          style: TextStyle(
-                              color  : Colors.white,
-                              fontSize: 30
-                          )
-                      ) ,
-                    )
 
-                ),
+                  Expanded(
+                    flex: 3,
+                    child: Column(
+                      children: [
+
+                        Expanded(
+
+                          child: Column(
+                            children: [
+
+                              Expanded(
+                                child: Row(
+                                  children: [
+
+                                    Expanded(
+                                      child: Column(
+                                        children: [
+                                          _customContainer(
+                                            number: 1,
+                                            color: Colors.green,
+                                          ),
+                                        _customContainer(
+                                          number: 1,
+                                          color: Colors.blue,
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+
+                                    _customContainer(
+                                      number: 2,
+                                      color: Colors.brown,
+                                    ),
+
+
+                                  ],
+                                ),
+                              ),
+
+
+
+
+
+
+                            ],
+                          ),
+                        ),
+
+
+
+                        _customContainer(
+                          number: 3,
+                          color: Colors.purple,
+                        ),
+                      ],
+                    ),
+                  ),
+
+
+
+    ],
             ),
-          ],
+            ),
+           ],
+            ),
         ),
             ),
 
 
-        ),
         );
 
   }
+  }
+
+
+Widget _customContainer( {
+  required int number,
+  required Color color,
+}){
+
+return Expanded(
+flex: number,
+child:Container(
+constraints: BoxConstraints.expand(),
+color: color,
+child: Center(
+child: Text('${number}',
+style: TextStyle(
+color  : Colors.white,
+fontSize: 30
+
+)
+)
+),
+
+),
+
+);
 }
+
